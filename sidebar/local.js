26d5adac082e8e3eca290138f7664c44d20e39c0
tuxifreund <kaiser.barbarossa@yandex.com>
2021-01-30 16:35:33 +0100

    Redirect to the browser’s language Wikipedia version

---
+++ sidebar/local.js
@@ -0,0 +1,57 @@
+if (navigator.language == 'ar') {
+	window.location = "https://ar.m.wikipedia.org/"
+}
+else if (navigator.language == 'de' ) {
+	window.location = "https://de.m.wikipedia.org/"
+}
+else if (navigator.language == 'en') {
+	window.location = "https://en.m.wikipedia.org/"
+}
+else if (navigator.language == 'es') {
+	window.location = "https://es.m.wikipedia.org/"
+}
+else if (navigator.language == 'fr') {
+	window.location = "https://fr.m.wikipedia.org/"
+}
+else if (navigator.language == 'it') {
+	window.location = "https://it.m.wikipedia.org/"
+}
+else if (navigator.language == 'arz') {
+	window.location = "https://arz.m.wikipedia.org/"
+}
+else if (navigator.language == 'nl') {
+	window.location = "https://nl.m.wikipedia.org/"
+}
+else if (navigator.language == 'ja') {
+	window.location = "https://ja.m.wikipedia.org/"
+}
+else if (navigator.language == 'pl') {
+	window.location = "https://pl.m.wikipedia.org/"
+}
+else if (navigator.language == 'ru') {
+	window.location = "https://ru.m.wikipedia.org/"
+}
+else if (navigator.language == 'ceb') {
+	window.location = "https://ceb.m.wikipedia.org/"
+}
+else if (navigator.language == 'sv') {
+	window.location = "https://sv.m.wikipedia.org/"
+}
+else if (navigator.language == 'uk') {
+	window.location = "https://uk.m.wikipedia.org/"
+}
+else if (navigator.language == 'vi') {
+	window.location = "https://vi.m.wikipedia.org/"
+}
+else if (navigator.language == 'ja') {
+	window.location = "https://ja.m.wikipedia.org/"
+}
+else if (navigator.language == 'war') {
+	window.location = "https://war.m.wikipedia.org/"
+}
+else if (navigator.language == 'zh') {
+	window.location = "https://zh.m.wikipedia.org/"
+}
+else {
+	window.location = "nowiki.html"
+}
