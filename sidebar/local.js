5116c99d2666c31b33b4681d290cbe09b9508818
KaiserBarbarossa <kaiser.barbarossa@yandex.com>
2021-08-21 11:02:45 +0200

    Use an array instead of huge if statements

--- sidebar/local.js
+++ sidebar/local.js
@@ -1,58 +1,15 @@
 // supported wikipedias: {ar, de, en, es, fr, it, arz, nl, ja, pl, ru, ceb, sv, uk, vi, ja, war, zh}.m.wikipedia.org
-if (navigator.language == 'ar') {
-	window.location = "https://ar.m.wikipedia.org/"
+
+const lang = ['ar', 'de', 'en', 'es', 'fr', 'it', 'arz', 'nl', 'ja', 'pl', 'ru',
+    'ceb', 'sv', 'uk', 'vi', 'ja', 'war', 'zh'];
+
+let select = '';
+for (let i in lang) {
+    if (navigator.language == lang[i]) {
+        window.location = 'https://' + lang[i] + '.m.wikipedia.org'
+        select = lang[i]
+    }
 }
-else if (navigator.language == 'de' ) {
-	window.location = "https://de.m.wikipedia.org/"
-}
-else if (navigator.language == 'en') {
-	window.location = "https://en.m.wikipedia.org/"
-}
-else if (navigator.language == 'es') {
-	window.location = "https://es.m.wikipedia.org/"
-}
-else if (navigator.language == 'fr') {
-	window.location = "https://fr.m.wikipedia.org/"
-}
-else if (navigator.language == 'it') {
-	window.location = "https://it.m.wikipedia.org/"
-}
-else if (navigator.language == 'arz') {
-	window.location = "https://arz.m.wikipedia.org/"
-}
-else if (navigator.language == 'nl') {
-	window.location = "https://nl.m.wikipedia.org/"
-}
-else if (navigator.language == 'ja') {
-	window.location = "https://ja.m.wikipedia.org/"
-}
-else if (navigator.language == 'pl') {
-	window.location = "https://pl.m.wikipedia.org/"
-}
-else if (navigator.language == 'ru') {
-	window.location = "https://ru.m.wikipedia.org/"
-}
-else if (navigator.language == 'ceb') {
-	window.location = "https://ceb.m.wikipedia.org/"
-}
-else if (navigator.language == 'sv') {
-	window.location = "https://sv.m.wikipedia.org/"
-}
-else if (navigator.language == 'uk') {
-	window.location = "https://uk.m.wikipedia.org/"
-}
-else if (navigator.language == 'vi') {
-	window.location = "https://vi.m.wikipedia.org/"
-}
-else if (navigator.language == 'ja') {
-	window.location = "https://ja.m.wikipedia.org/"
-}
-else if (navigator.language == 'war') {
-	window.location = "https://war.m.wikipedia.org/"
-}
-else if (navigator.language == 'zh') {
-	window.location = "https://zh.m.wikipedia.org/"
-}
-else {
+if (select = '') {
 	window.location = "nowiki.html"
 }
